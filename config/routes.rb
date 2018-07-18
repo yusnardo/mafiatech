8277ab7eb7cca03aed99c200bfbfe0c93f1332bd
Yusnardo Tendio <yus21newbie@gmail.com>
2018-07-18 19:49:25 +0700

    #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  root to: redirect("#{I18n.default_locale}", status: 302), as: :redirected_root 
-  scope '(:locale)' , :locale => /en|id/ do
-  	root 'home#index'
-  	
-  	get 'course', to: ("course#index")
-  	get 'blog', to: ("blog#index")
-  end
+	root to: redirect("#{I18n.default_locale}", status: 302), as: :redirected_root 
+	#resources :session, only: [:create, :destroy]
+	#get '/session/new', to: redirect('/id/login')
+	scope '(:locale)' , :locale => /en|id/ do
+	  	root 'home#index'
+	  	
+	  	get 'course', to: ("course#index")
+	  	get 'blog', to: ("blog#index")
+	  	#get '/sessions/new', to: redirect('/%{locale}/login')
+
+	  	#get '/login', to: 'sessions#new', as: 'login'
+	    #get '/logout', to: 'sessions#destroy', as: 'logout'
+
+	    # resources :sessions, only: [:create] do
+	    #     collection do
+	    #       get :oauth
+	    #     end
+	    # end
+  	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
